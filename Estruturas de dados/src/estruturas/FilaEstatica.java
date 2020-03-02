8a94646efc17fdd55a027743322a573ad6b79298
Gabriel Da Rocha Souza <40413843@redecorp.br>
2020-03-02 15:30:32 -0300

    Update no pacote 'estruturas'

---
+++ Estruturas de dados/src/estruturas/FilaEstatica.java
@@ -0,0 +1,59 @@
+package estruturas;
+
+import java.util.Arrays;
+
+public class FilaEstatica {
+	
+	private int tamanho;
+	private int fim;
+	private int[] dados;
+	private int tamMaximo;
+	
+	FilaEstatica(int lim) {
+		tamanho = 0;
+		fim = -1;
+		tamMaximo = lim;
+		dados = new int[lim];
+	}
+	
+	private boolean cheia() {
+		return (tamanho == tamMaximo);
+	}
+	
+	private boolean vazia() {
+		return (tamanho == 0);
+	}
+	
+	//enqueue
+	public void adiciona(int n) {
+		if(!cheia()) {
+			tamanho++;
+			fim++;
+			dados[fim] = n;
+		} else {
+			System.out.println("Erro ao adicionar! A fila está cheia!");
+		}
+	}
+	
+	//dequeue
+	public int remove() {
+		int r = -1;;
+		if(!vazia()) {
+			tamanho--;
+			r = dados[0];
+			for(int i = 0; i < tamanho; i++) {
+				dados[i] = dados[i + 1];
+			}
+			dados[fim] = 0;
+			fim--;
+		} else {
+			System.out.println("Erro ao remover! A fila está vazia!");
+		}
+		return r;
+	}
+	
+	public void mostraFila() {
+		System.out.println("Fila --> " + Arrays.toString(dados));
+	}
+}
+
